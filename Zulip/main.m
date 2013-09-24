b26af2e22c91eee55661a6bbca41501773a57286
Leo Franchi <leo@zulip.com>
2013-09-24 15:56:09 -0400

    Remove XCode DP5 hack

--- Zulip/main.m
+++ Zulip/main.m
@@ -1,34 +1,7 @@
-
 #import <UIKit/UIKit.h>
-
-
-// clean the console output. Only for XCode 5 DP 5. REMOVE ME
-
-typedef int (*PYStdWriter)(void *, const char *, int);
-
-static PYStdWriter _oldStdWrite;
-
-int __pyStderrWrite(void *inFD, const char *buffer, int size);
-void __iOS7B5CleanConsoleOutput(void);
-
-int __pyStderrWrite(void *inFD, const char *buffer, int size)
-{
-    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
-        return 0;
-    }
-    return _oldStdWrite(inFD, buffer, size);
-}
-
-void __iOS7B5CleanConsoleOutput(void)
-{
-    _oldStdWrite = stderr->_write;
-    stderr->_write = __pyStderrWrite;
-}
-
 
 int main(int argc, char *argv[])
 {
-    __iOS7B5CleanConsoleOutput();
     @autoreleasepool {
         int retVal = UIApplicationMain(argc, argv, nil, nil);
         return retVal;
